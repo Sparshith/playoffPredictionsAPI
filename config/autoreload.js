876b078cecd3e33e906581bdc6dade55c7798e93
Sparshith Sampath <sparshithsampath@gmail.com>
2018-05-10 06:54:46 +0000

    new packages

---
+++ config/autoreload.js
@@ -0,0 +1,15 @@
+module.exports.autoreload = {
+  active: true,
+  usePolling: false,
+  dirs: [
+    "api/models",
+    "api/controllers",
+    "api/services",
+    "config/locales",
+	"config/routes.js"
+  ],
+  ignored: [
+    // Ignore all files with .ts extension
+    "**.ts"
+  ]
+};
